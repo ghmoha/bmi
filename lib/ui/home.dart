bc43309d379b2bc5dce27230a45eca0dd70f9057
Ghazi <ghazi1987@gmail.com>
2019-02-08 00:33:56 +0300

    add bmi methods

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,6 +6,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  double bmi = 0;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -19,28 +25,85 @@ class _HomeState extends State<Home> {
         alignment: Alignment.center,
         child: new ListView(
           children: <Widget>[
-            Image.asset("images/bmilogo.png", height:
-              90.0, width: 90.0),
+            Image.asset("images/bmilogo.png", height: 90.0, width: 90.0),
             new Container(
-              height: 180.0,
+              height: 200.0,
               width: 380.0,
               color: Colors.grey,
               child: new Column(
                 children: <Widget>[
                   new TextField(
                     keyboardType: TextInputType.numberWithOptions(),
-                    controller: ()=> debugPrint("test"),
                     decoration: new InputDecoration(
-                      hintText: "Your Age", icon: Icon(Icons.person),
+                      hintText: "Your Age",
+                      icon: Icon(Icons.person),
                     ),
+                  ),
+                  new TextField(
+                    keyboardType: TextInputType.numberWithOptions(),
+                    controller: _heightController,
+                    decoration: new InputDecoration(
+                      hintText: "Height in Feet",
+                      icon: Icon(Icons.assessment),
+                    ),
+                  ),
+                  new TextField(
+                    keyboardType: TextInputType.numberWithOptions(),
+                    controller: _weightController,
+                    decoration: new InputDecoration(
+                      hintText: "Weight in lb",
+                      icon: Icon(Icons.menu),
+                    ),
+                  ),
+                  new Padding(padding: EdgeInsets.all(10.5)),
+                  new Center(
+                    child: new Row(
+                      children: <Widget>[
 
+                        new Container(
+                          margin: const EdgeInsets.only(left: 150.0),
+                          child: new RaisedButton(
+                            onPressed: showBMI,
+                            color: Colors.pinkAccent,
+                            child: new Text(
+                              "BMI",
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 18.8)),
+                          ),
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
-            )
+            ),
+            new Padding(padding: EdgeInsets.all(5.5)),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text("Your BMI: $bmi",
+                  style: new TextStyle(
+                      color:Colors.blueAccent,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500),),
+                new Text("OverWeight",
+                  style: new TextStyle(
+                      color:Colors.red,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500),),
+
+
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+  void showBMI(){
+    setState(() {
+      bmi = ((double.parse(_weightController.text)) / ((double.parse(_heightController.text)) * (double.parse(_heightController.text))) * 703) ;
+    });
+
   }
 }
